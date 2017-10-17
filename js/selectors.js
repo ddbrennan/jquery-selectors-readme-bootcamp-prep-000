6138591aa1494d9e1b92e16301364c96e0d73a5a
David Brennan <ddbren@gmail.com>
2017-10-17 23:38:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+
+}
+
+function divSelector() {
+
+}
+
+function firstListItem() {
+
+}
